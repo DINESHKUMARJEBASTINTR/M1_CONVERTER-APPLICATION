bf67daff94c57cf249639b6cb1e2207127c86ae7
DINESHKUMAR JEBASTIN.T.R <101498911+DINESHKUMARJEBASTINTR@users.noreply.github.com>
2022-03-30 15:44:09 +0530

    Update bcd.h

--- 3_Implementation/Inc/bcd.h
+++ 3_Implementation/Inc/bcd.h
@@ -1,23 +1,11 @@
-/**
- * @file bcd.h
- * @author your name (you@domain.com)
- * @brief Header file for Binary Coded Decimal (BCD) method
- * @version 0.1
- * @date 2021-04-08
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #include <stdio.h>
 typedef struct Conversion_bcd
 {
     int bcd_op[50];
     int arr_l;
 } Conversion_bcd;
-/**
- * @brief Structures for storing the input & output stream
- * 
- */
+
 
 typedef struct Input_bcd
 {
@@ -25,12 +13,7 @@ typedef struct Input_bcd
 } Input_bcd;
 Conversion_bcd transformBcd(Input_bcd a);
 
-/**
- * @brief Function to convert a number from decimal to BCD system
- * 
- * @param a 
- * @return Conversion_bcd 
- */
+
 Conversion_bcd transformBcd(Input_bcd a)
 {
     int num = a.num;
